4b087ead39dc2f6e3844fca045d12a4df90d7912
prgopinath@gmail.com <O71516o9$>
2022-03-22 23:03:55 +0530

    Added github actions for AKS

---
+++ src/test/java/com/prg/todo/todoapp/TodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prg.todo.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
